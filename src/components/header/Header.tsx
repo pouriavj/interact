683396dbd0fef20940d6dca3486a35853ca0cdf8
Pouria <108181964+pouriavj@users.noreply.github.com>
2026-06-12 18:03:07 +0330

    Remove IconButtons Hover Effect

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,32 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import HeaderAuth from "./HeaderAuth";
 import styles from "./Header.module.css";
 
-function Logo() {
-  return (
-    <svg
-      width="36"
-      height="36"
-      viewBox="0 0 36 36"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-    >
-      <circle cx="18" cy="18" r="18" fill="currentColor" />
-    </svg>
-  );
-}
-
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link
-        href="/"
-        className={styles.logo}
-        aria-label="Home"
-      >
-        <Logo />
+      <Link href="/" className={styles.logo} aria-label="Home">
+        <Image
+          src="/interact-logo-lowQuality.png"
+          alt="Interact Logo"
+          width={98}
+          height={26}
+          priority
+        />
       </Link>
 
       <HeaderAuth />
