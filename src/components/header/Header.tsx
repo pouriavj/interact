209b24e8393b2bf38e5fe5dbb9ab5016ccffe437
Pouria <108181964+pouriavj@users.noreply.github.com>
2026-06-13 21:10:33 +0330

    Update theme

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,22 +2,26 @@ import Link from "next/link";
 import Image from "next/image";
 
 import HeaderAuth from "./HeaderAuth";
+import Stories from "@/components/stories/Stories"
 import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logo} aria-label="Home">
-        <Image
-          src="/interact_logo_transparent.png"
-          alt="Interact Logo"
-          width={102}
-          height={24}
-          priority
-        />
-      </Link>
+    <header className={styles.headerContainer}>
+      <div className={styles.header}>
+        <Link href="/" className={styles.logo} aria-label="Home">
+          <Image
+            src="/interact_logo_transparent.png"
+            alt="Interact Logo"
+            width={102}
+            height={24}
+            priority
+          />
+        </Link>
 
-      <HeaderAuth />
+        <HeaderAuth />
+      </div>
+      <Stories />
     </header>
   );
 }
---
+++ src/components/stories/Stories.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./Stories.module.css";
+
+const stories = [
+  { id: 1, name: "You", image: null },
+  { id: 2, name: "aurora", image: "/story1.jpg" },
+  { id: 3, name: "ericarl", image: "/story2.jpg" },
+  { id: 4, name: "keanuagl", image: "/story3.jpg" },
+  { id: 5, name: "awkarin", image: "/story4.jpg" },
+];
+
+export default function Stories() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        {stories.map((story) => (
+          <div key={story.id} className={styles.story}>
+            <div className={styles.avatarWrapper}>
+              {story.image ? (
+                <Image
+                  src={story.image}
+                  alt={story.name}
+                  width={56}
+                  height={56}
+                  className={styles.avatar}
+                />
+              ) : (
+                <div className={styles.you}>
+                  <span className={styles.plus}>+</span>
+                </div>
+              )}
+            </div>
+
+            <span className={styles.name}>{story.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
