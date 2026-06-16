e7b95a64238ff936c00b147021c00cf480505d26
Pouria <108181964+pouriavj@users.noreply.github.com>
2026-06-16 15:58:26 +0330

    Update Verify-Request Page Ui

--- src/app/verify-request/page.tsx
+++ src/app/verify-request/page.tsx
@@ -1,23 +1,39 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { Mail } from "lucide-react";
+
 import styles from "@/components/login/Login.module.css";
+import Link from "next/link";
+import { paths } from "@/paths";
 
 export default async function VerifyRequestPage() {
   const session = await auth();
 
-  // If user is already logged in → redirect immediately
   if (session) {
     redirect("/");
   }
 
   return (
     <main className={styles.page}>
-      <div className={styles.card}>
-        <h1>Check your inbox</h1>
+      <div className={styles.verifyCard}>
+        <div className={styles.verifyIconWrapper}>
+          <Mail className={styles.verifyIcon} />
+        </div>
 
-        <p>We've sent you a secure sign-in link.</p>
+        <h1 className={styles.verifyTitle}>Check your inbox</h1>
 
-        <p>Open the email and click the link to continue.</p>
+        <p className={styles.verifyText}>
+          We’ve sent you a secure sign-in link.
+        </p>
+
+        <p className={styles.verifySubtext}>
+          Open your email and click the link to continue.
+        </p>
+
+        <div className={styles.verifyHint}>
+          Didn’t receive it? Check spam or
+          <Link href={paths.login()}>try again</Link>.
+        </div>
       </div>
     </main>
   );
